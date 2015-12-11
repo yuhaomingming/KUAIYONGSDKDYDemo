702430b6acb4056a580e00c2ac9d7c4066c317a3
于浩 <351945755@qq.com>
2015-12-11 17:51:44 +0800

    fix

---
+++ Common/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  QuickUnifyPlatformDemo
+//
+//  Created by yuhao on 15/6/15.
+//  Copyright (c) 2015年 yuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    BOOL _isHidden;
+}
+@property (weak, nonatomic) IBOutlet UITextView *nofTextView;
+
+@end
+
