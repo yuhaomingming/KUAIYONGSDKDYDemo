8d416259fb66c9e585e923c7f535ed75c7007697
于浩 <351945755@qq.com>
2015-12-11 14:50:44 +0800

    Del

--- DYSDK/KUAIYONGSDK/QuickUnifyPlatformDemo/build/QuickUnifyPlatformDemo.build/Release-iphoneos/QuickUnifyPlatformDemo.build/Objects-normal/arm64/ViewController.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/Common/Source/Demo/ViewController.m \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/Common/Source/Demo/ViewController.h \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/DYSDK/KUAIYONGSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/KUAIYONGSDK/DYFramework/QuickUnifyPlatform.framework/Headers/QuickPlatformName.h \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/DYSDK/KUAIYONGSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/KUAIYONGSDK/DYFramework/QuickUnifyPlatform.framework/Headers/QuickConstant.h \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/DYSDK/KUAIYONGSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/KUAIYONGSDK/DYFramework/QuickUnifyPlatform.framework/Headers/QuickProductInfo.h \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/DYSDK/KUAIYONGSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/KUAIYONGSDK/DYFramework/QuickUnifyPlatform.framework/Headers/QuickUnifyPlatform.h \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/DYSDK/KUAIYONGSDK/QuickUnifyPlatformDemo/../../../Common/SDK/Platform/KUAIYONGSDK/DYFramework/QuickUnifyPlatform.framework/Modules/module.modulemap
--- DYSDK/KUAIYONGSDK/QuickUnifyPlatformDemo/build/QuickUnifyPlatformDemo.build/Release-iphoneos/QuickUnifyPlatformDemo.build/Objects-normal/armv7/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/Common/Source/Demo/main.m \
-  /Users/yuhao/Documents/QuickUnify/Quick/QuickSDK/Common/Source/Demo/AppDelegate.h
